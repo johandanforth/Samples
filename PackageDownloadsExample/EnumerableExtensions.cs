2c7ef37c8406e7c93a3071a168a177452cb9f448
Johan Danforth <johan_danforth@hotmail.com>
2022-05-10 00:30:06 +0200

    more work

---
+++ PackageDownloadsExample/EnumerableExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace PackageDownloadsExample
+{
+	/// <summary>
+	/// </summary>
+	public static class EnumerableExtensions
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="asyncAction"></param>
+		/// <param name="maxDegreeOfParallelism"></param>
+		/// <param name="cancellationToken"></param>
+		/// <typeparam name="T"></typeparam>
+		public static async Task ParallelForEachAsync<T>(this IEnumerable<T> source,
+			Func<T, CancellationToken, Task> asyncAction, int maxDegreeOfParallelism,
+			CancellationToken cancellationToken)
+		{
+			var throttler = new SemaphoreSlim(maxDegreeOfParallelism);
+			var tasks = source.Select(async item =>
+			{
+				await throttler.WaitAsync(cancellationToken);
+				if (cancellationToken.IsCancellationRequested)
+					return;
+
+				try
+				{
+					await asyncAction(item, cancellationToken).ConfigureAwait(false);
+				}
+				finally
+				{
+					throttler.Release();
+				}
+			});
+
+			await Task.WhenAll(tasks);
+		}
+	}
+}
--- PackageDownloadsExample/Program.cs
+++ PackageDownloadsExample/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Collections.Concurrent;
 
 using NuGet.Common;
 using NuGet.Configuration;
@@ -14,148 +9,188 @@ using NuGet.Versioning;
 
 namespace PackageDownloadsExample
 {
-	/// <summary>
-	///     Find the download count for each version of a package.
-	/// </summary>
-	public class Program
-	{
-		private const string DownloadDirectory = "/nuget-downloads/";
-		private static readonly List<string> Handled = new List<string>();
-		private static readonly SourceCacheContext SourceCacheContext = new SourceCacheContext();
-		private static ConsoleColor _defaultColor;
+    /// <summary>
+    ///     Find the download count for each version of a package.
+    /// </summary>
+    public class Program
+    {
+        private const string DownloadDirectory = "/nuget-downloads/";
+        private static readonly ConcurrentBag<string> Handled = new ConcurrentBag<string>();
+        private static readonly SourceCacheContext SourceCacheContext = new SourceCacheContext();
+        private static ConsoleColor _defaultColor;
+        private static readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
 
-		/// <summary>
-		///     Find the download count for each version of a package.
-		/// </summary>
-		/// <param name="packageId">The package ID. Example: "newtonsoft.json"</param>
-		public static async Task Main(string packageId, bool wide = false)
-		{
-			if (string.IsNullOrEmpty(packageId))
-			{
-				Console.WriteLine(
-					"The --package-id option is required. Run --help for more information");
+        /// <summary>
+        ///     Start
+        /// </summary>
+        /// <param name="packageId"></param>
+        /// <param name="exactMatch"></param>
+        public static async Task Main(string packageId, bool exactMatch = false)
+        {
+            if (string.IsNullOrEmpty(packageId))
+            {
+                Console.WriteLine(
+                    "The --package-id option is required. Run --help for more information");
 
-				return;
-			}
+                return;
+            }
 
-			_defaultColor = Console.ForegroundColor;
+            _defaultColor = Console.ForegroundColor;
 
-			if (!Directory.Exists(DownloadDirectory))
-				Directory.CreateDirectory(DownloadDirectory);
+            if (!Directory.Exists(DownloadDirectory))
+                Directory.CreateDirectory(DownloadDirectory);
 
 
-			// https://www.meziantou.net/exploring-the-nuget-client-libraries.htm
+            // https://www.meziantou.net/exploring-the-nuget-client-libraries.htm
 
-			var source = new PackageSource("https://api.nuget.org/v3/index.json");
-			var providers = Repository.Provider.GetCoreV3();
-			var repository = new SourceRepository(source, providers);
+            var repository = GetNugetRepository();
 
-			await HandlePackage(packageId, repository, 0, wide);
+            await HandlePackage(packageId, repository, 0, exactMatch, null);
 
-			Console.ForegroundColor = _defaultColor;
-		}
+            Console.ForegroundColor = _defaultColor;
 
-		private static async Task HandlePackage(string packageId, SourceRepository repository, int level,
-			bool isFirst = false, string version = null)
-		{
-			var search = await repository.GetResourceAsync<RawSearchResourceV3>();
+            Console.WriteLine($"{Handled.Count} packages handled!");
+        }
 
-			var filter = new SearchFilter(false, SearchFilterType.IsLatestVersion)
-			{
-				SupportedFrameworks = new List<string>
-				{
-					"net48", 
-					"net5.0",
-					//"net6.0"
-				}
-			};
+        private static SourceRepository GetNugetRepository()
+        {
+            var source = new PackageSource("https://api.nuget.org/v3/index.json");
+            var providers = Repository.Provider.GetCoreV3();
+            var repository = new SourceRepository(source, providers);
+            return repository;
+        }
 
-			//	var query = isFirst ? $"id:\"{packageId}\"" : $"package-id:\"{packageId}\"";
+        private static async Task HandlePackage(string packageId, SourceRepository repository,
+            int level, bool exactMatch, string version)
+        {
+	        if (!string.IsNullOrEmpty(version))
+	        {
+		        var packageAndVersion = packageId + "." + version;
+		        if (Handled.Contains(packageAndVersion))
+			        return;
 
-			var response = await search.Search(packageId, filter, 0, 200, NullLogger.Instance,
-				CancellationToken.None);
+		        Handled.Add(packageAndVersion);
+            }
 
-			var results = response.Select(result => result.ToObject<SearchResult>()).ToList();
+            var search = await repository.GetResourceAsync<RawSearchResourceV3>();
 
-			// https://docs.microsoft.com/en-us/nuget/reference/nuget-client-sdk
+            var filter = new SearchFilter(false, SearchFilterType.IsLatestVersion)
+            {
+                SupportedFrameworks = new List<string> { "net48", "net5.0", "net6.0" }
+            };
 
-			results = isFirst
-				? results.Where(r =>
-						r.PackageId.ToLowerInvariant().StartsWith(packageId.ToLowerInvariant()))
-					.ToList()
-				: results.Where(r => r.PackageId == packageId).ToList();
+            var response = await search.Search(packageId, filter, 0, 1000, NullLogger.Instance,
+                CancellationToken.None);
 
-			foreach (var result in results)
-			{
-				for (int i = 0; i < level; i++)
-					Console.Write("\t");
+            var results = response.Select(result => result.ToObject<SearchResult>()).ToList();
 
-				Console.ForegroundColor = ConsoleColor.DarkGray;
-				Console.Write($" {result.PackageId} {result.Versions.Last().Version} ");
+            // https://docs.microsoft.com/en-us/nuget/reference/nuget-client-sdk
 
-				var resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+            results = exactMatch
+                ? results.Where(r => r.PackageId.ToLowerInvariant() == packageId.ToLowerInvariant())
+                    .ToList()
+                : results.Where(r =>
+                        r.PackageId.ToLowerInvariant().StartsWith(packageId.ToLowerInvariant()))
+                    .ToList();
 
-				var packageName = $"{result.PackageId}.{result.Versions.Last().Version}";
-				var filePath = Path.Combine(DownloadDirectory, packageName + ".nupkg");
+            await Parallel.ForEachAsync(results, new ParallelOptions { MaxDegreeOfParallelism = 1 },
+                async (result, token) =>
+                {
+                    var tabs = "";
+                    for (var i = 0; i < level; i++)
+                        tabs += "  ";
 
-				if (!File.Exists(filePath))
-				{
-					Console.ForegroundColor = ConsoleColor.Yellow;
-					Console.Write("downloading... ");
-					await DownloadPackageFile(filePath, resource, result);
-					Console.ForegroundColor = ConsoleColor.Green;
-					Console.WriteLine("done!");
+                    //TODO: Manage minimum required version
 
-					if (!Handled.Contains(result.PackageId))
-						Handled.Add(result.PackageId);
+                    //dependencies
+                    var dependencies = await GetPackageDependencies(repository, result,
+                        SourceCacheContext, new CancellationToken());
 
-					//dependencies
-					var deps = await GetDependencies(resource, result, SourceCacheContext,
-						new CancellationToken());
+                    dependencies.RemoveAll(d =>
+                        !d.TargetFramework.Framework.ToLowerInvariant().StartsWith(".net"));
 
-					var packages = deps
-						.Where(d =>
-							d.TargetFramework.Framework.ToLowerInvariant().StartsWith(".net"))
-						.SelectMany(d => d.Packages).Distinct().
-						Where(p => !Handled.Contains(p.Id));
+                    var packages = dependencies.SelectMany(d => d.Packages).ToList();
+                    packages.RemoveAll(p => Handled.Contains(p.Id + "." + p.VersionRange.MinVersion.ToNormalizedString()));
+                    packages = packages.DistinctBy(p => new { p.Id, p.VersionRange.MinVersion }).ToList();
 
-					foreach (var package in packages)
-						if (!Handled.Contains(package.Id))
-						{
-							await HandlePackage(package.Id, repository, level+1);
-						}
-				}
-				else
-				{
-					Console.ForegroundColor = ConsoleColor.DarkGray;
-					Console.WriteLine("already downloaded.");
-				}
+                    if (packages.Any())
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine(
+                            $"{tabs}{packages.Count} unhandled dependencies for {result.PackageId}");
 
-			}
-		}
+                        //foreach (var package in packages)
+                        await Parallel.ForEachAsync(packages,
+                            new ParallelOptions { MaxDegreeOfParallelism = 1 },
+                            async (package, token) =>
+                            {
+                                var version = package.VersionRange.MinVersion.ToNormalizedString();
+                                await HandlePackage(package.Id, repository, level + 1, true, version);
+                            });
+                    }
 
-		private static async Task DownloadPackageFile(string downloadPath,
-			FindPackageByIdResource resource, SearchResult result)
-		{
-			await using var fileStream = File.OpenWrite(downloadPath);
-			await resource.CopyNupkgToStreamAsync(result.PackageId,
-				new NuGetVersion(result.Versions.Last().Version), fileStream, SourceCacheContext,
-				NullLogger.Instance, CancellationToken.None);
-		}
+                    var packageWithVersion = $"{result.PackageId}.{result.Versions.Last().Version}";
+                    var filePath = Path.Combine(DownloadDirectory, packageWithVersion + ".nupkg");
 
-		private static async Task<IEnumerable<PackageDependencyGroup>> GetDependencies(
-			FindPackageByIdResource resource, SearchResult result, SourceCacheContext cache,
-			CancellationToken cancellationToken)
-		{
-			await using var packageStream = new MemoryStream();
-			await resource.CopyNupkgToStreamAsync(result.PackageId,
-				new NuGetVersion(result.Versions.Last().Version), packageStream, cache,
-				NullLogger.Instance, CancellationToken.None);
+                    await semaphoreSlim.WaitAsync();
+                    try
+                    {
+                        if (!File.Exists(filePath))
+                        {
+                            try
+                            {
+                                await DownloadPackageFile(filePath, repository, result);
 
-			using var packageReader = new PackageArchiveReader(packageStream);
-			var nuspecReader = await packageReader.GetNuspecReaderAsync(cancellationToken);
-			var deps = nuspecReader.GetDependencyGroups();
-			return deps;
-		}
-	}
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine($"{tabs}{packageWithVersion} downloaded!");
+
+
+                            }
+                            catch (Exception e)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine($"Error: {e.Message}");
+                            }
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.WriteLine($"{tabs}{packageWithVersion} already downloaded.");
+                        }
+                    }
+                    finally
+                    {
+                        semaphoreSlim.Release();
+                    }
+                });
+        }
+
+        private static async Task DownloadPackageFile(string downloadPath,
+            SourceRepository repository, SearchResult result)
+        {
+            var resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+
+            await using var fileStream = File.OpenWrite(downloadPath);
+            await resource.CopyNupkgToStreamAsync(result.PackageId,
+                new NuGetVersion(result.Versions.Last().Version), fileStream, SourceCacheContext,
+                NullLogger.Instance, CancellationToken.None);
+        }
+
+        private static async Task<List<PackageDependencyGroup>> GetPackageDependencies(
+            SourceRepository repository, SearchResult result, SourceCacheContext cache,
+            CancellationToken cancellationToken)
+        {
+            var resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+
+            await using var packageStream = new MemoryStream();
+            await resource.CopyNupkgToStreamAsync(result.PackageId,
+                new NuGetVersion(result.Versions.Last().Version), packageStream, cache,
+                NullLogger.Instance, CancellationToken.None);
+
+            using var packageReader = new PackageArchiveReader(packageStream);
+            var nuspecReader = await packageReader.GetNuspecReaderAsync(cancellationToken);
+            var dependencies = nuspecReader.GetDependencyGroups();
+            return dependencies.ToList();
+        }
+    }
 }
